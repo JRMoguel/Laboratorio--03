4a3c94d4954806ffb16c31570564d329a90b7603
JRMoguel <jrmoguel@correo.url.edu.gt>
2026-02-05 19:52:57 -0600

    Datos
    Datos del estudiate y compañero

--- Laboratorio/Program.cs
+++ Laboratorio/Program.cs
@@ -2,8 +2,8 @@
 Console.WriteLine("==========================================");
 Console.WriteLine("Laboratorio # 03");
 // Ingresar aquí nombre y carnet del dueño del repositorio
-
+Console.WriteLine("José Rodrigo Moguel Obregón 1047426");
 // Ingresar aquí nombre y carnet del colaborador:
-
+Console.WriteLine("Luis Daniel Oliva Hernandez 1194026");
 Console.WriteLine("==========================================");
 Console.ReadLine();
